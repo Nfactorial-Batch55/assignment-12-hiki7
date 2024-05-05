494f7c06a5da1b0884e8b8506cdd11ef6c766217
hiki7 <rsaliyev03@gmail.com>
2024-05-05 21:55:14 +0500

    hw_bonus.py: ex2 is solved

--- hw_bonus.py
+++ hw_bonus.py
@@ -32,7 +32,8 @@ Example:
 triple_combinations([1, 2, 3, 4]) -> [(1, 2, 3), (1, 2, 4), (1, 3, 4), (2, 3, 4)]
 """
 def triple_combinations(lst: List[int]) -> List[Tuple[int, int, int]]:
-    pass
+    n = len(lst)
+    return [(i, j, k) for i in lst for j in lst for k in lst if i < j < k]
 
 """
 Exercise-3: Nested Dictionary Comprehension
